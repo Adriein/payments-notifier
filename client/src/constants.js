734a8b96fb6b0caff91256211965d651afc2195e
Adriein <adria.claret@gmail.com>
2020-12-30 13:07:36 +0100

    final release

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,9 +1,6 @@
 export const PRO = 'PRO';
 export const DEV = 'DEV';
 
-export const PRO_URL = 'https://https://pay-notifier.herokuapp.com/api/v1/';
-export const DEV_URL = 'http://localhost:3000/api/v1/';
-
 export const SUCCESS = 'ok';
 export const ERROR = 'error';
 export const DEFAULT = '';
