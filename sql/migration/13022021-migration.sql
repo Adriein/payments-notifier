925f846dbc4c485c9859f8c0815793b0e0ee4525
Adriein <adria.claret@gmail.com>
2021-03-07 10:12:43 +0100

    added migration

--- sql/migration/13022021-migration.sql
+++ sql/migration/13022021-migration.sql
@@ -17,4 +17,6 @@ UPDATE subscriptions SET pricing='{"trimestral":{"duration":90,"price":150}}' WH
 
 ALTER TABLE subscriptions ADD COLUMN active BOOLEAN;
 
+INSERT INTO subscriptions VALUES ('55bfc474-8c4b-474a-8ff3-b23433a40a64', '{"anual":{"duration":365,"price":1000}}', 'Sat Feb 27 2021 15:51:21 GMT+0100 (hora estándar de Europa central)', false, false, 'ba8740ef-c7eb-44cc-a114-77b17404ea34', true);
+
 UPDATE subscriptions SET active=true;
