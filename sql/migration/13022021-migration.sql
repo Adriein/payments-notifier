2b980052ea949cfa4cf1cacfdc5413effc477dfc
Adriein <adria.claret@gmail.com>
2021-03-07 10:17:48 +0100

    more migrations

--- sql/migration/13022021-migration.sql
+++ sql/migration/13022021-migration.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS app_config (
     user_id VARCHAR REFERENCES users(id) ON DELETE CASCADE
 );
 
+INSERT INTO app_config (id, pricing, warning_delay, defaulter_delay, email_content, user_id) VALUES ('6b9902f6-4d22-4441-a1f1-c987d6fa9000', '{"mensual":{"duration":30,"price":50},"trimestral":{"duration":90,"price":150}}', 5, 2, '', 'ba8740ef-c7eb-44cc-a114-77b17404ea34');
+
 ALTER TABLE users ADD COLUMN owner_id VARCHAR(50);
 
 UPDATE users SET owner_id='ba8740ef-c7eb-44cc-a114-77b17404ea34';
@@ -17,6 +19,6 @@ UPDATE subscriptions SET pricing='{"trimestral":{"duration":90,"price":150}}' WH
 
 ALTER TABLE subscriptions ADD COLUMN active BOOLEAN;
 
-INSERT INTO subscriptions VALUES ('55bfc474-8c4b-474a-8ff3-b23433a40a64', '{"anual":{"duration":365,"price":1000}}', 'Sat Feb 27 2021 15:51:21 GMT+0100 (hora estándar de Europa central)', false, false, 'ba8740ef-c7eb-44cc-a114-77b17404ea34', true);
+INSERT INTO subscriptions (id, pricing, payment_date, warned, notified, user_id) VALUES ('55bfc474-8c4b-474a-8ff3-b23433a40a64', '{"anual":{"duration":365,"price":1000}}', 'Sat Feb 27 2021 15:51:21 GMT+0100 (hora estándar de Europa central)', false, false, 'ba8740ef-c7eb-44cc-a114-77b17404ea34', true);
 
 UPDATE subscriptions SET active=true;
