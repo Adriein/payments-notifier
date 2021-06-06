abc449260768c976e1d37030f447f4911aadf149
Adriein <adria.claret@gmail.com>
2021-06-06 18:58:15 +0200

    landing wip

--- client/src/pages/Landing/LandingPage.js
+++ client/src/pages/Landing/LandingPage.js
@@ -1,19 +1,91 @@
 import React from 'react';
 import * as Styled from './Styles';
+import Button from '../../shared/components/Button/Button';
+
+import onlinePaymentsImg from '../../img/undraw_online_payments_luau.svg'
+import dietImg from '../../img/undraw_healthy_options_sdo3.svg'
+import exercicesImg from '../../img/undraw_fitness_stats_sht6.svg'
 
 const LandingPage = () => {
   return (
-    <Styled.Hero>
-      <Styled.HeroTitle>
-        La solución integral para profesionales del
-        <Styled.HeroTitleEmphasis>
-          deporte y la alimentación
-        </Styled.HeroTitleEmphasis>
-      </Styled.HeroTitle>
-      <Styled.HeroSubtitle>
-        
-      </Styled.HeroSubtitle>
-    </Styled.Hero>
+    <Styled.LandingPage>
+      <Styled.HeroWrapper>
+        <Styled.Hero>
+          <Styled.HeroDescription>
+            <Styled.HeroTitle>
+              La solución integral para profesionales del
+              <Styled.HeroTitleEmphasis>
+                deporte y la alimentación
+              </Styled.HeroTitleEmphasis>
+            </Styled.HeroTitle>
+            <Styled.HeroSubtitle>
+              Gestiona todo lo relacionado con tu negocio de una forma comoda y
+              eficiente. Nutrilog es la manera mas rápida de poder llevar mas
+              clientes sin perder calidad en tus asesorias.
+            </Styled.HeroSubtitle>
+            <Styled.HeroActionButtons>
+              <Button type="submit" variant="primary">
+                Registrate
+              </Button>
+              <Button type="submit" variant="secondary">
+                Contacta con nostros
+              </Button>
+            </Styled.HeroActionButtons>
+          </Styled.HeroDescription>
+          <Styled.HeroVideo>
+            <iframe
+              width="400"
+              height="200"
+              src={`https://www.youtube.com/embed/EZzbh9YjVhQ?list=RDEZzbh9YjVhQ`}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Embedded youtube"
+            />
+          </Styled.HeroVideo>
+        </Styled.Hero>
+      </Styled.HeroWrapper>
+      <Styled.WhyWrapper>
+        <Styled.WhyHero>
+          <Styled.WhyTitle>
+            ¿Qué hace Nutri<Styled.WhyTitleSpan>log</Styled.WhyTitleSpan>{' '}
+            especial?
+          </Styled.WhyTitle>
+          <Styled.WhyCard>
+            <Styled.WhyCardMedia src={onlinePaymentsImg}/>
+            <Styled.WhyCardTitle>PAGOS</Styled.WhyCardTitle>
+            <Styled.WhyCardSubtitle success>ACTIVO</Styled.WhyCardSubtitle>
+            <Styled.WhyCardContent>
+              Nutrilog te ayuda a gestionar los pagos de tus clientes, controla
+              quien ha pagado y cuando. Cuando la suscripción de esa persona
+              haya caducado recibira un mail de pre aviso, tanto el contenido
+              del mail como el momento en el que se manda es 100% configurable
+            </Styled.WhyCardContent>
+          </Styled.WhyCard>
+          <Styled.WhyCard>
+          <Styled.WhyCardMedia src={dietImg}/>
+            <Styled.WhyCardTitle>DIETAS </Styled.WhyCardTitle>
+            <Styled.WhyCardSubtitle>EN DESARROLLO</Styled.WhyCardSubtitle>
+            <Styled.WhyCardContent>
+              Con Nutrilog puedes elaborar y gestionar las dietas de tus
+              clientes. Usa las guias interactivas y las herramientas que te
+              proporciona la aplicacion para elaborar dietas lo mas
+              eficientemente posible y mas adaptadas a tus clientes
+            </Styled.WhyCardContent>
+          </Styled.WhyCard>
+          <Styled.WhyCard>
+          <Styled.WhyCardMedia src={exercicesImg}/>
+            <Styled.WhyCardTitle>EJERCICIOS</Styled.WhyCardTitle>
+            <Styled.WhyCardSubtitle>EN DESARROLLO</Styled.WhyCardSubtitle>
+            <Styled.WhyCardContent>
+              Pautar ejercicios y guiar a tus clientes para realizarlos
+              correctamente nunca fue tan fácil, con Nutrilog recibes las
+              herramientas adecuadas para hacer progresar a tus clientes
+            </Styled.WhyCardContent>
+          </Styled.WhyCard>
+        </Styled.WhyHero>
+      </Styled.WhyWrapper>
+    </Styled.LandingPage>
   );
 };
 
--- client/src/pages/Landing/Styles.js
+++ client/src/pages/Landing/Styles.js
@@ -1,10 +1,30 @@
 import styled from 'styled-components';
 import { COLORS } from '../../shared/utils/colors';
 
-export const Hero = styled.div`
-  background-color: ${COLORS.backgroundWhite};
-  display: flex;
+export const LandingPage = styled.div`
+  overflow-x: hidden;
 `;
+
+export const HeroWrapper = styled.div`
+  background-color: ${COLORS.backgroundWhite};
+  padding: 50px;
+`;
+
+export const Hero = styled.div`
+  display: flex;
+  margin-left: 230px;
+  margin-right: 230px;
+`;
+
+export const HeroDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 50%;
+  margin-right: 100px;
+`;
+
+export const HeroVideo = styled.div``;
 
 export const HeroTitle = styled.p`
   font-size: 30px;
@@ -16,5 +36,62 @@ export const HeroTitleEmphasis = styled.p`
 `;
 
 export const HeroSubtitle = styled.p`
-    
+  color: ${COLORS.textLight};
+`;
+
+export const HeroActionButtons = styled.div`
+  padding: 5px 0px 5px 0px;
+  display: flex;
+  width: 280px;
+  justify-content: space-between;
+`;
+
+export const WhyWrapper = styled.div`
+  padding: 20px 50px 20px 50px;
+  display: flex;
+  justify-content: center;
+  color: ${COLORS.textMedium};
+`;
+
+export const WhyTitle = styled.h2`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  font-weight: 500;
+`;
+
+export const WhyTitleSpan = styled.span`
+  color: ${COLORS.primary};
+  margin-right: 6px;
+`;
+
+export const WhyCard = styled.div`
+  padding: 60px 20px 40px 20px;
+  width: 300px;
+  height: 500px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const WhyHero = styled(Hero)`
+  flex-wrap: wrap;
+  justify-content: space-around;
+`;
+
+export const WhyCardTitle = styled.h3`
+  margin-bottom: 10px;
+  font-weight: 500;
+`;
+
+export const WhyCardSubtitle = styled.h5`
+  margin-bottom: 10px;
+  color: ${(props) => (props.success ? COLORS.success : COLORS.textMedium)};
+`;
+
+export const WhyCardContent = styled.article``;
+
+export const WhyCardMedia = styled.img`
+  width: 100%;
+  height: 150px;
+  margin-bottom: 20px;
 `;
