639b0999548381ec07bc1ed9282d198fe6636f02
Adriein <adria.claret@gmail.com>
2021-11-21 16:31:31 +0100

    report generation

---
+++ prisma/migrations/20211121150702_init/migration.sql
@@ -0,0 +1,104 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "owner_id" TEXT NOT NULL,
+    "role_id" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL,
+    "created_at" TIMESTAMP(0) NOT NULL,
+    "updated_at" TIMESTAMP(0) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pricing" (
+    "id" TEXT NOT NULL,
+    "pricing_name" TEXT NOT NULL,
+    "duration" SMALLINT NOT NULL,
+    "amount" SMALLINT NOT NULL,
+    "user_id" TEXT,
+    "created_at" TIMESTAMP(0) NOT NULL,
+    "updated_at" TIMESTAMP(0) NOT NULL,
+
+    CONSTRAINT "pricing_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "subscription" (
+    "id" TEXT NOT NULL,
+    "payment_date" TIMESTAMP(0) NOT NULL,
+    "warned" BOOLEAN NOT NULL,
+    "notified" BOOLEAN NOT NULL,
+    "active" BOOLEAN NOT NULL,
+    "expired" BOOLEAN NOT NULL,
+    "pricing_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(0) NOT NULL,
+    "updated_at" TIMESTAMP(0) NOT NULL,
+
+    CONSTRAINT "subscription_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "role" (
+    "id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "created_at" TIMESTAMP(0) NOT NULL,
+    "updated_at" TIMESTAMP(0) NOT NULL,
+
+    CONSTRAINT "role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "config" (
+    "id" TEXT NOT NULL,
+    "language" TEXT NOT NULL,
+    "send_notifications" BOOLEAN NOT NULL,
+    "send_warnings" BOOLEAN NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(0) NOT NULL,
+    "updated_at" TIMESTAMP(0) NOT NULL,
+
+    CONSTRAINT "config_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "app_config" (
+    "id" TEXT NOT NULL,
+    "warning_delay" SMALLINT NOT NULL,
+    "notification_delay" SMALLINT NOT NULL,
+    "email_content" TEXT NOT NULL,
+    "last_sent_report" TIMESTAMP(0) NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(0) NOT NULL,
+    "updated_at" TIMESTAMP(0) NOT NULL,
+
+    CONSTRAINT "app_config_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "config_user_id_key" ON "config"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "app_config_user_id_key" ON "app_config"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscription" ADD CONSTRAINT "subscription_pricing_id_fkey" FOREIGN KEY ("pricing_id") REFERENCES "pricing"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "subscription" ADD CONSTRAINT "subscription_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "config" ADD CONSTRAINT "config_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "app_config" ADD CONSTRAINT "app_config_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
