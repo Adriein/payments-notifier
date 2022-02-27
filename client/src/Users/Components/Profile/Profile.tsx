445d1f8df340b6bfbc9e9a24f50038375eec35c2
Adriein <adria.claret@gmail.com>
2022-02-27 15:05:05 +0100

    creating client profile view

--- client/src/Users/Components/Profile/Profile.tsx
+++ client/src/Users/Components/Profile/Profile.tsx
@@ -20,12 +20,14 @@ import { useTranslation } from "react-i18next";
 import ProfileForm from "./ProfileForm";
 import { useToggle } from "../../../Shared/Hooks/useToggle";
 import { UsersContext } from "../../Context/UsersContext";
+import useBooleanBeautifier from "../../../Shared/Hooks/useBooleanBeautifier";
+import ProfileClientInfo from "./ProfileClientInfo/ProfileClientInfo";
 
 const Profile = ({ id }: UserProfileProps) => {
   const { state, fetchClientProfile } = useContext(UsersContext);
   const { t } = useTranslation('profile');
   const [ edit, toggleEdit ] = useToggle();
-
+  
   useEffect(() => {
     (async () => {
       await fetchClientProfile({ clientId: id });
@@ -45,35 +47,7 @@ const Profile = ({ id }: UserProfileProps) => {
 
               </StyledUserResume>
             </StyledUserResumeContainer>
-            <StyledEditableUserInfoTitle>
-              <StyledDetailsTag>
-                <FiChevronDown/>
-                <p>{t('details')}</p>
-              </StyledDetailsTag>
-              <StyledEdit onClick={toggleEdit}>{t('edit')}</StyledEdit>
-            </StyledEditableUserInfoTitle>
-            {edit ? <ProfileForm user={state.clientProfile}/> : (
-              <StyledUserDetailsInfoContainer>
-                <StyledUserDetailTitle type={"subtitle"} bold>{t('account_details')}</StyledUserDetailTitle>
-                <StyledUserDetailInfo>{state.clientProfile.username}</StyledUserDetailInfo>
-                <StyledUserDetailInfo>{state.clientProfile.email}</StyledUserDetailInfo>
-                <StyledUserDetailTitle type={"subtitle"} bold>{t('billing_email')}</StyledUserDetailTitle>
-                <StyledUserDetailInfo>{state.clientProfile.email}</StyledUserDetailInfo>
-              </StyledUserDetailsInfoContainer>
-            )}
-            <StyledEditableUserInfoTitle>
-              <StyledDetailsTag>
-                <FiChevronDown/>
-                <p>Configuración</p>
-              </StyledDetailsTag>
-              <StyledEdit onClick={toggleEdit}>{t('edit')}</StyledEdit>
-            </StyledEditableUserInfoTitle>
-            <StyledUserDetailsInfoContainer>
-              <StyledUserDetailTitle type={"subtitle"} bold>Detalles de la configuración</StyledUserDetailTitle>
-              <StyledUserDetailInfo>{state.clientProfile.config.language}</StyledUserDetailInfo>
-              <StyledUserDetailInfo>{state.clientProfile.config.role}</StyledUserDetailInfo>
-              <StyledUserDetailInfo>{state.clientProfile.config.sendWarnings}</StyledUserDetailInfo>
-            </StyledUserDetailsInfoContainer>
+            <ProfileClientInfo client={state.clientProfile}/>
           </StyledPersonalInfoContainer>
           <StyledPersonalSubscriptionInfo>
             <StyledPersonalSubscriptionInfoNavigation>
---
+++ client/src/Users/Components/Profile/ProfileClientInfo/ProfileClientInfo.tsx
@@ -0,0 +1,58 @@
+import { ProfileClientInfoProps } from "./ProfileClientInfoProps";
+import * as Accordion from '@radix-ui/react-accordion';
+import { FiChevronDown } from "react-icons/fi";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import {
+  StyledAccordionContent,
+  StyledAccordionHeader, StyledAccordionRoot, StyledAccordionTrigger, StyledContent, StyledContentTitle,
+} from "./Styles";
+import { StyledEdit } from "../Styles";
+import { useToggle } from "../../../../Shared/Hooks/useToggle";
+import useBooleanBeautifier from "../../../../Shared/Hooks/useBooleanBeautifier";
+
+const ProfileClientInfo = ({ client }: ProfileClientInfoProps) => {
+  const { t } = useTranslation('profile');
+  const [ edit, toggleEdit ] = useToggle();
+  const { beautify } = useBooleanBeautifier({
+    isTrue: 'enabled',
+    isFalse: 'disabled'
+  });
+
+  return (
+    <StyledAccordionRoot type="multiple" defaultValue={[ "item-1", "item-2" ]}>
+      <Accordion.Item value="item-1">
+        <StyledAccordionHeader>
+          <StyledAccordionTrigger>
+            <FiChevronDown/>
+            <span>{t('details')}</span>
+          </StyledAccordionTrigger>
+          <StyledEdit onClick={toggleEdit}>{t('edit')}</StyledEdit>
+        </StyledAccordionHeader>
+        <StyledAccordionContent>
+          <StyledContentTitle type={"subtitle"} bold>{t('account_details')}</StyledContentTitle>
+          <StyledContent>{client.username}</StyledContent>
+          <StyledContent>{client.email}</StyledContent>
+        </StyledAccordionContent>
+      </Accordion.Item>
+      <Accordion.Item value="item-2">
+        <StyledAccordionHeader>
+          <StyledAccordionTrigger>
+            <FiChevronDown/>
+            <span>{t('config')}</span>
+          </StyledAccordionTrigger>
+          <StyledEdit onClick={toggleEdit}>{t('edit')}</StyledEdit>
+        </StyledAccordionHeader>
+        <StyledAccordionContent>
+          <StyledContentTitle type={"subtitle"} bold>{t('config_language')}</StyledContentTitle>
+          <StyledContent>{client.config.language}</StyledContent>
+          <StyledContentTitle type={"subtitle"} bold>{t('config_notifications')}</StyledContentTitle>
+          <StyledContent>{beautify(client.config.sendNotifications)}</StyledContent>
+        </StyledAccordionContent>
+      </Accordion.Item>
+    </StyledAccordionRoot>
+  );
+}
+
+
+export default ProfileClientInfo;
