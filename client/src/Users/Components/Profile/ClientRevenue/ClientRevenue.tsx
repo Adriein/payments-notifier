5706eac25ff333700e49aec4598ba38dec4b2aee
Adriein <adria.claret@gmail.com>
2022-03-15 19:45:13 +0100

    adding client revenue traductions

--- client/src/Users/Components/Profile/ClientRevenue/ClientRevenue.tsx
+++ client/src/Users/Components/Profile/ClientRevenue/ClientRevenue.tsx
@@ -3,22 +3,24 @@ import Text from "../../../../Shared/Components/Text";
 import React from "react";
 import { ClientRevenueProps } from "./ClientRevenueProps";
 import useDateFormatter from "../../../../Shared/Hooks/useDateFormatter";
+import { useTranslation } from "react-i18next";
 
 const ClientRevenue = ({ revenue }: ClientRevenueProps) => {
+  const { t } = useTranslation('profile');
   const { simplify } = useDateFormatter();
 
   return (
     <StyledUserResume>
       <StyledProfileResumePart>
-        <Text type={"subtitle"}>Spent</Text>
+        <Text type={"subtitle"}>{t('resume_spent')}</Text>
         <p>{revenue.spent}</p>
       </StyledProfileResumePart>
       <StyledProfileResumePart part={"middle"}>
-        <Text type={"subtitle"}>Since</Text>
+        <Text type={"subtitle"}>{t('resume_since')}</Text>
         <p>{simplify(revenue.since)}</p>
       </StyledProfileResumePart>
       <StyledProfileResumePart>
-        <Text type={"subtitle"}>Mrr</Text>
+        <Text type={"subtitle"}>{t('resume_recurrent_income')}</Text>
         <p>{revenue.monthlyRecurringRevenue}</p>
       </StyledProfileResumePart>
     </StyledUserResume>
